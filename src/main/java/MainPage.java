39d7fb39fbc4d04b8fc4b5f74346548b5127ce17
Mohamed Shabeer KP <mohammedshabeerkp@gmail.com>
2019-08-03 00:16:16 +0530

    v3.0 browser upgradation

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -227,33 +227,14 @@ public class MainPage extends javax.swing.JFrame {
     aFrame.setSize(screenSize.width, screenSize.height);
 }
 
-    
     private void btn_browserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_browserActionPerformed
     createContent("www.google.com");  
     }//GEN-LAST:event_btn_browserActionPerformed
 
  public  void createContent(String link) {   
-     JourneyBrowserView browser = new JourneyBrowserView("https://google.com");
-        //JFrame frame = new JFrame();
+     JourneyBrowserView browser = new JourneyBrowserView("https://google.com");   
         browser_panel.add(browser, BorderLayout.CENTER);
-
-     //   frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-        /*frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                browser.getCefApp().dispose();
-                frame.dispose();
-            }
-        });
-
-        frame.setTitle("Journey");
-        frame.setSize(1000, 600);
-        frame.setVisible(true);*/
         browser_panel.updateUI();
-
-    // Stop listening to origPanel (prevent dangling reference).
-    
-      
 }
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
